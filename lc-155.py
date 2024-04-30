257c635273c05ccb378ea4581e92bc8ebb0fc0ee
Shashank 'Zarth' Raghunathan <shashank.raghunathan95@gmail.com>
2024-04-30 01:30:59 -0400

    29th April 2024 commit by Shashank 'Zarth' Raghunathan

---
+++ lc-155.py
@@ -0,0 +1,29 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        min_val = val if len(self.min_stack) == 0 or val < self.min_stack[-1] else self.min_stack[-1]
+        self.min_stack.append(min_val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+    
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
